3d46a310317cae03d30ce6dadf37c41e6c9a5f9b
Rita00 <rita.lapao00@gmail.com>
2021-09-29 18:35:04 +0100

    script para gerar pets e owners, and file

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import random
+
 OwnersId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-            30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49]
+            30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56]
 
 OwnersNames = ["Maria", "Jose", "Antonio", "Joao", "Francisco", "Ana", "Luiz", "Paulo", "Carlos", "Manuel", "Pedro",
                "Francisca", "Marcos", "Raimundo", "Sebastiao", "Antonia", "Marcelo", "Jorge", "Marcia", "Geraldo",
                "Adriana", "Sandra", "Fernando", "Fabio", "Roberto", "Marcio", "Edson", "Andre", "Sergio", "Josefa",
                "Patricia", "Daniel", "Rodrigo", "Rafael", "Joaquim", "Vera", "Ricardo", "Eduardo", "Terezinha", "Sonia",
-               "Alexandre", "Rita", "Luciana", "Claudio", "Rosa", "Benedito", "Leandro", "Raimunda", "Mario"]
+               "Alexandre", "Rita", "Luciana", "Claudio", "Rosa", "Benedito", "Leandro", "Raimunda", "Mario", "Marta",
+               "Joana", "Filipa", "Raul", "Anabela", "Jonas", "Nelson"]
 
 OwnersBirth = ["2000-01-12", "1970-01-13", "1976-05-20", "1986-10-20", "1993-01-12", "1960-12-13", "2003-09-04",
                "1970-01-13", "1956-11-30", "1999-04-20", "1970-06-31", "1989-10-01", "2001-12-05", "2000-05-07",
@@ -16,7 +19,8 @@ OwnersPhone = [967453621, 927512346, 919067004, 931237750, 960056026, 963316745,
                915871175, 964047195, 933486949, 920356059, 933675324, 912963912, 965085585, 921122718, 938662800,
                919658770, 968983749, 926113395, 915134235, 930107810, 960908307, 921844896, 930082344, 915514712,
                961265245, 923992956, 915539083, 933400629, 965628082, 927167727, 918028853, 930436624, 968699423,
-               924901175, 918777319, 935302554, 961336669]
+               924901175, 918777319, 935302554, 961336669, 939854678, 917622343, 937765564, 967392875, 934533434,
+               923323123, 965588746]
 
 OwnersAddress = ["Rua Brigadeiro Lino Valente", "Rua Cortinhas Fonte", "Estrada Abrantes", "Rua Muro Bacalhoeiros",
                  "Rua Sao Salvador", "Avenida Boavista", "Rua Vasco Gama", "Rua Condes Torre", "Rua São Romao",
@@ -27,7 +31,9 @@ OwnersAddress = ["Rua Brigadeiro Lino Valente", "Rua Cortinhas Fonte", "Estrada 
 PetsId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
           31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
           58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84,
-          85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+          85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
+          110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131,
+          132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 141, 142, 142, 143, 144, 145, 146, 147, 148, 149, 150]
 
 PetsNames = ["Sherlock", "Diana", "Aladdin", "Bambi", "Fiona", "Hulk", "Charles", "Merlin", "Harry", "Tarzan", "Scooby",
              "Hermione", "Mafalda", "Ariel", "Nemo", "Yoda", "Bolt", "Prenda", "Homer", "Thunder", "Percy", "Will",
@@ -38,7 +44,11 @@ PetsNames = ["Sherlock", "Diana", "Aladdin", "Bambi", "Fiona", "Hulk", "Charles"
              "Claire", "Lily", "Andie", "Meredith", "Charlotte", "Aurora", "Sol", "Nile", "Luna", "Gio", "Malu",
              "Minnie", "Joy", "Filipa", "Pedrita", "Tina", "Alegria", "Flor", "Matilde", "Angelina", "Margot", "Ariel",
              "Patty", "Penny", "Bonnie", "Alice", "Dalila", "Bela", "Stella", "Mittens", "Maya", "Xica", "Mel", "Mila",
-             "Amora"]
+             "Amora", "Daphne", "Da Vinci", "Dara", "Dolores", "Dakota", "Dadinho", "Delta", "Dexter", "Dinho", "Denis",
+             "Dada", "Dudu", "Bernard", "Logan", "Gustaf", "Josh", "Alfredo", "Rob", "Peter", "Lex", "Nolan", "Monet",
+             "Phillip", "Mordecai", "Jack", "Jake", "Oliver", "Pietro", "Samurai", "Singer", "Cameron", "Darryl",
+             "Norman", "Rosita", "Ash", "Buster", "Guy", "Rambo", "Petris", "Vicente", "Mao", "Eddie", "Koda", "Caleb",
+             "Fluffy", "Pirata", "Stallone", "Marshall", "Stuart", "Ronald"]
 
 PetsSpecies = ["Gato", "Cao", "Tartaruga", "Peixe", "Canario", "Ovelha", "Porco", "Hamster", "Cavalo", "Coelho",
                "Chinchila", "Papagaio", "Cobra", "Pato", "Arara"]
@@ -51,20 +61,74 @@ PetsBirth = ["2015-01-12", "2020-01-13", "2017-05-20", "2016-10-20", "2021-01-12
              "2017-01-13", "2020-11-30", "2021-04-20", "2016-06-31", "2014-10-01", "2018-12-05", "2020-05-07",
              "2015-06-02", "2018-11-25", "2019-01-23", "2021-05-20", "2015-06-18", "2020-08-20", "2020-09-05"]
 
-PetsDescription = []
+PetsDescription = ["Animal com crescimento normal", "Aparenta ter artroses, devido ao envelhecimento",
+                   "Pelo macio e suave", 'Animal de porte pequeno', "Animal saudavel e que gosta de brincar"]
 
 
-def random_file():
-    print()
+def random_file(out_file, numOwners):
+    f = open(out_file, 'w')
+    phone_used = list()
+    name_used = list()
+    id_pet_used = list()
+    name_pet_used = list()
+
+    all_info = list()
+
+    for p in range(numOwners):
+        pets_list = list()
+        name = random.choice(OwnersNames)
+        while name in name_used:
+            name = random.choice(OwnersNames)
+        name_used.append(name)
+
+        birth_owner = random.choice(OwnersBirth)
+
+        phone = random.choice(OwnersPhone)
+        while phone in phone_used:
+            phone = random.choice(OwnersPhone)
+        phone_used.append(phone)
+
+        address = random.choice(OwnersAddress)
+
+        how_many_pets = random.randint(1, 2)
+        for pet in range(how_many_pets):
+            id_pet = random.choice(PetsId)
+            while id_pet in id_pet_used:
+                id_pet = random.choice(PetsId)
+            id_pet_used.append(id_pet)
+
+            name_pet = random.choice(PetsNames)
+            while name_pet in name_pet_used:
+                name_pet = random.choice(PetsNames)
+            name_pet_used.append(name_pet)
+
+            specie = random.choice(PetsSpecies)
+
+            gender = random.choice(PetsGender)
+
+            weight = random.choice(PetsWeight)
+
+            birth = random.choice(PetsBirth)
+
+            description = random.choice(PetsDescription)
+
+            pets_list.append((id_pet, name_pet, specie, gender, weight, birth, description))
+
+        all_info.append((OwnersId[p], name, birth_owner, phone, address, pets_list))
+
+    for p in all_info:
+        f.write("%d#%s#%s#%d#%s#" % (p[0], p[1], p[2], p[3], p[4]))
+
+        print("%d#%s#%s#%d#%s#" % (p[0], p[1], p[2], p[3], p[4]))
+
+        for n in p[5]:
+            f.write("%d+%s+%s+%s+%.2f+%s+%s?" % (n[0], n[1], n[2], n[3], n[4], n[5], n[6]))
+            print("%d+%s+%s+%s+%d+%s+%s?" % (n[0], n[1], n[2], n[3], n[4], n[5], n[6]))
+        f.write("\n")
+    f.close()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    output_file = "file.txt"
+    num_owners = 55
+    random_file(output_file, num_owners)
